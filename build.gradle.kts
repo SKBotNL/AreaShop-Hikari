b260f76430056562b2b5d0b51f23482113f143d0
SKBotNL <skbotnl@skiff.com>
2024-02-03 19:40:21 +0100

    Update AreaShop to 2.7.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ repositories {
 dependencies {
     compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly("com.ghostchu:quickshop-api:6.0.0.0")
-    compileOnly(files("libs/AreaShop-2.7.16.jar"))
+    compileOnly(files("libs/AreaShop-2.7.17.jar"))
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
 
     implementation("com.ghostchu.quickshop.compatibility:common:5.2.0.10")
